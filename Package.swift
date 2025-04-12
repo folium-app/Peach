ff347203637a0b8d209e9e565282192dd76cc7e0
Jarrod Norwell <official.antique@gmail.com>
2025-04-12 08:19:09 +0700

    Updated files

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "Peach",
     platforms: [
-        .iOS(.v15),
-        .tvOS(.v15)
+        .iOS(.v16),
+        .tvOS(.v16)
     ],
     products: [
         .library(name: "Peach", targets: ["Peach"]),
